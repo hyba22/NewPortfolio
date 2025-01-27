84c4b75f4a8a7764035e0becdad8a45c7815007e
Hyba <bouslahihiba55@gmail.com>
2025-01-27 13:08:22 +0100

    New style commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './contact.module.css';
+import styles from './Contact.module.css';
 import { MdEmail } from "react-icons/md";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
- 
+
 const Contact = () => {
   return (
     <footer className={styles.container} id='contact'>
@@ -12,21 +12,21 @@ const Contact = () => {
         <p>Feel free to reach out!</p>
       </div>
       <div className={styles.listContainer}>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <MdEmail className={styles.icon} /> 
-          <a href="mailto:bouslahihiba55@gmail.com">bouslahihiba55@gmail.com</a>
-        </li>
-        <li className={styles.link}> 
-          <BsLinkedin className={styles.icon} /> 
-          <a href="https://www.linkedin.com/in/hiba-bouslahi-44b84b197/">linkedin.com/in/hiba-bouslahi</a>
-        </li>
-        <li className={styles.link}> 
-          <FaGithub className={styles.icon} /> 
-          <a href="https://github.com/hyba22">github.com/hyba22</a>
-        </li>
-      </ul>
-    </div>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <MdEmail className={styles.icon} /> 
+            <a href="mailto:bouslahihiba55@gmail.com" className={styles.linkText}>bouslahihiba55@gmail.com</a>
+          </li>
+          <li className={styles.link}> 
+            <BsLinkedin className={styles.icon} /> 
+            <a href="https://www.linkedin.com/in/hiba-bouslahi-44b84b197/" className={styles.linkText}>linkedin.com/in/hiba-bouslahi</a>
+          </li>
+          <li className={styles.link}> 
+            <FaGithub className={styles.icon} /> 
+            <a href="https://github.com/hyba22" className={styles.linkText}>github.com/hyba22</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
